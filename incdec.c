52f60c938f26cec658e2a96279d94c2a9531cd5e
anoban <2016sb117@univ.jfn.ac.lk>
2023-12-27 13:43:08 +0530

    deleted:    .vscode/settings.json 	modified:   incdec.c

--- incdec.c
+++ incdec.c
@@ -1,18 +1,19 @@
 // code example for increment-decrement
 
 #include <pthread.h>
-#include <sched.h>  // header for schedulers
+#include <sched.h>      // header for schedulers
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h> // EXIT_SUCCESS
+#include <stdlib.h>     // EXIT_SUCCESS
 
-#define NTHREADS   40LLU
-#define UPPERBOUND 10LLU
+#define NTHREADS   6LLU // as long as the thread count is an even number, sum will be 0 ultimately.
+#define UPPERBOUND 100LLU
+// as we increase these numbers, the chances for data races increase and the likelihood of getting a non-zero sum increases.
 
-static volatile pthred_t threads[NTHREADS];
+static volatile pthread_t threads[NTHREADS];
 // we really do not need to use a struct to thread indices
-static volatile uint64_t threadIndices[NTHREADS];
-static volatile uint64_t sum = 0; // hotspot for data races
+static volatile uint64_t  threadIndices[NTHREADS];
+static volatile uint64_t  sum = 0; // hotspot for data races
 
 static void* __attribute((noinline)) TaskInc(void* thread_props) {
     const uint64_t* const restrict threadId = (uint64_t*) thread_props;
@@ -35,6 +36,15 @@ static void* __attribute((noinline)) TaskDec(void* thread_props) {
 // Linux typically load balances around every 200 milliseconds.
 
 int main(void) {
+    for (size_t i = 0; i < NTHREADS; ++i) threadIndices[i] = i;
+
+    // make threads with even ids do incrementation
+    for (size_t i = 0; i < NTHREADS; i += 2) pthread_create(threads + i, NULL, TaskInc, (void*) (threadIndices + i));
+    // make threads with even ids do decrementation
+    for (size_t i = 1; i < NTHREADS; i += 2) pthread_create(threads + i, NULL, TaskDec, (void*) (threadIndices + i));
+    for (size_t i = 1; i < NTHREADS; ++i) pthread_join(threads[i], NULL);
+
+    printf("Finally sum is %lu\n", sum);
     puts("Completed!");
     return EXIT_SUCCESS;
 }
