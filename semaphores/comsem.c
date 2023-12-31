a2f8140d6297132c3e8099d7bc7f4bf8c6a44f33
anoban <2016sb117@univ.jfn.ac.lk>
2023-12-31 13:23:59 +0530

    modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   .vscode/tasks.json 	modified:   semaphores/comsem.c

--- semaphores/comsem.c
+++ semaphores/comsem.c
@@ -1,13 +1,32 @@
 // a tad more complex use of semaphore
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define NTHREADS 1000LLU
-static volatile int64_t global[NTHREADS] = {0};
-static pthread_t threads[NTHREADS] = {0};
-static volatile sem_t semaphore= {0};
+static volatile int64_t global[NTHREADS]  = { 0 };
+static pthread_t        threads[NTHREADS] = { 0 };
+static volatile sem_t   semaphore         = { 0 }; // initialize with 1
 
 // let's implement a kernel that executes a time consuming task, instead of a simple increment
+static void* __attribute((noinline)) Kernel(void* input) {
+    const int64_t thread_id = *((int64_t*) (input));
+    sem_wait(&semaphore);
+    for (size_t i = 0; i < thread_id; ++i) global[thread_id]++;
+    sem_post(&semaphore);
+    return NULL;
+}
+
+int main(void) {
+    for (size_t i = 0; i < NTHREADS; ++i) threads[i] = i;
+    for (size_t i = 0; i < NTHREADS; ++i) pthread_create(threads + i, NULL, Kernel, global + i);
+    for (size_t i = 0; i < NTHREADS; ++i) pthread_join(threads[i], NULL);
+
+    int64_t sum = 0;
+    for (size_t i = 0; i < NTHREADS; ++i) sum += global[i];
+    wprintf_s("Sum is %lld\n", sum);
+    return EXIT_SUCCESS;
+}
